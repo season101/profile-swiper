3625abe4578b835013f30677fe26bb5f3af1abbb
Sijan Malla <sijan.malla101@gmail.com>
2023-09-13 10:29:31 -0500

    Modified Carousel using Slick

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,12 +1,38 @@
+import { FaQuoteRight } from 'react-icons/fa';
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-
-// import Slider from 'react-slick';
-// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-// import { list } from './data';
+import { list } from './data';
 
 const SlickCarousel = () => {
-  return <h2>Slick Slider</h2>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    fade: true,
+    slidesToScroll: 1,
+  };
+  return (
+    <section className="slick-container">
+      <Slider {...settings}>
+        {list.map(({ id, image, name, quote, title }) => {
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </Slider>
+    </section>
+  );
 };
 
 export default SlickCarousel;
